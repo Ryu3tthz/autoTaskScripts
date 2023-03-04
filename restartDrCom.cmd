67972e15fa23161ccf9842d7aae6123a7e3029b3
Primimy <38491737+Primimy@users.noreply.github.com>
2023-03-04 09:51:39 +0800

    fix bug

--- restartDrCom.cmd
+++ restartDrCom.cmd
@@ -1,2 +1,2 @@
 taskkill /F /IM pythonw.exe
-"C:\Users\primi\AppData\Local\Programs\Python\Python27" "C:\Users\primi\Documents\autoTaskScripts\latest-wired.pyw"
+start "" "C:\Users\primi\AppData\Local\Programs\Python\Python27\pythonw.exe" "C:\Users\primi\Documents\autoTaskScripts\latest-wired.pyw"

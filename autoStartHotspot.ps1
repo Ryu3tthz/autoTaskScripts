ed1f5d73b30e87c4c15afe0c31d41e184c03a423
Primimy <38491737+Primimy@users.noreply.github.com>
2023-05-28 13:50:14 +0800

    change ? to Where-Object

--- autoStartHotspot.ps1
+++ autoStartHotspot.ps1
@@ -1,5 +1,5 @@
 Add-Type -AssemblyName System.Runtime.WindowsRuntime
-$asTaskGeneric = ([System.WindowsRuntimeSystemExtensions].GetMethods() | ? { $_.Name -eq 'AsTask' -and $_.GetParameters().Count -eq 1 -and $_.GetParameters()[0].ParameterType.Name -eq 'IAsyncOperation`1' })[0]
+$asTaskGeneric = ([System.WindowsRuntimeSystemExtensions].GetMethods() | Where-Object { $_.Name -eq 'AsTask' -and $_.GetParameters().Count -eq 1 -and $_.GetParameters()[0].ParameterType.Name -eq 'IAsyncOperation`1' })[0]
 Function Await($WinRtTask, $ResultType) {
     $asTask = $asTaskGeneric.MakeGenericMethod($ResultType)
     $netTask = $asTask.Invoke($null, @($WinRtTask))
@@ -7,7 +7,7 @@ Function Await($WinRtTask, $ResultType) {
     $netTask.Result
 }
 Function AwaitAction($WinRtAction) {
-    $asTask = ([System.WindowsRuntimeSystemExtensions].GetMethods() | ? { $_.Name -eq 'AsTask' -and $_.GetParameters().Count -eq 1 -and !$_.IsGenericMethod })[0]
+    $asTask = ([System.WindowsRuntimeSystemExtensions].GetMethods() | Where-Object { $_.Name -eq 'AsTask' -and $_.GetParameters().Count -eq 1 -and !$_.IsGenericMethod })[0]
     $netTask = $asTask.Invoke($null, @($WinRtAction))
     $netTask.Wait(-1) | Out-Null
 }

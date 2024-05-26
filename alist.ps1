5af094ddd279fb13e3336ce09f43d4284cdbdb3b
Primimy <38491737+Primimy@users.noreply.github.com>
2024-05-26 08:23:59 +0800

    update alist

--- alist.ps1
+++ alist.ps1
@@ -1,2 +1,15 @@
 Set-Location "C:\Programs\alist\"
+$processName = "alist"
+$process = Get-Process -Name $processName -ErrorAction SilentlyContinue
+if ($process) {
+    $process | Stop-Process -Force
+}
 .\alist.exe server
+start-sleep -s 5
+Set-Location "C:\Programs\rclone\"
+$processName = "rclone"
+$process = Get-Process -Name $processName -ErrorAction SilentlyContinue
+if ($process) {
+    $process | Stop-Process -Force
+}
+.\rclone.exe mount Alist:/ Z: --vfs-cache-mode full --cache-dir C:/Programs/rclone/Temp  --network-mode
--- rclone.ps1
+++
@@ -1,7 +0,0 @@
-Set-Location "C:\Programs\rclone\"
-$processName = "rclone"
-$process = Get-Process -Name $processName -ErrorAction SilentlyContinue
-if ($process) {
-    $process | Stop-Process -Force
-}
-.\rclone.exe mount Alist:/ Z: --vfs-cache-mode full --cache-dir C:/Programs/rclone/Temp  --network-mode
